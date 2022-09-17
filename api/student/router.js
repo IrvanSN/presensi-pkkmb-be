e2fb1f163fb393997a49718cf82b132f34ac7798
IrvanSN <30742772+IrvanSN@users.noreply.github.com>
2022-09-17 09:39:53 +0700

    fix delete student

--- api/student/router.js
+++ api/student/router.js
@@ -23,8 +23,8 @@ router.post('/all/from/group', masterAuth, getAllStudentFromGroup);
 router.get('/count/from/group', masterAuth, getStudentCountFromGroup);
 router.post('/add', masterAuth, addStudent);
 router.put('/:id/update', masterAuth, updateStudent);
+router.delete('/:id/delete', singleAuth, deleteStudent);
 // router.get('/all', masterAuth, getAllStudent);
 // router.get('/:id', getStudentById);
-// router.delete('/:id/delete', deleteStudent);
 
 module.exports = router;
